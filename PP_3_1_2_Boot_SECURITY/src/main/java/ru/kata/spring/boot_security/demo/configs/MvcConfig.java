446fdad4bbe3853af5d4586b70beb29efdae7a33
dashaokulevich <119444815+dashaokulevich@users.noreply.github.com>
2023-03-10 03:52:00 +0300

    Update MvcConfig.java

--- PP_3_1_2_Boot_SECURITY/src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
+++ PP_3_1_2_Boot_SECURITY/src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
@@ -17,13 +17,4 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/user").setViewName("user");
     }
 
-    @Bean
-    public DataSourceInitializer dataSourceInitializer(@Qualifier("dataSource") final DataSource dataSource) {
-        ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
-        resourceDatabasePopulator.addScript(new ClassPathResource("/data.sql"));
-        DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
-        dataSourceInitializer.setDataSource(dataSource);
-        dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
-        return dataSourceInitializer;
-    }
 }
